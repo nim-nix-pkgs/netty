aa0b82c28364fea05a8845b01a6a5fa41c451494
hugosenari <hugosenari@gmail.com>
2022-05-12 11:54:22 -0300

    chore: re index netty

--- flake.nix
+++ flake.nix
@@ -7,6 +7,38 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."netty-master".dir   = "master";
+  inputs."netty-master".owner = "nim-nix-pkgs";
+  inputs."netty-master".ref   = "master";
+  inputs."netty-master".repo  = "netty";
+  inputs."netty-master".type  = "github";
+  inputs."netty-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."netty-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."netty-0_2_0".dir   = "0_2_0";
+  inputs."netty-0_2_0".owner = "nim-nix-pkgs";
+  inputs."netty-0_2_0".ref   = "master";
+  inputs."netty-0_2_0".repo  = "netty";
+  inputs."netty-0_2_0".type  = "github";
+  inputs."netty-0_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."netty-0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."netty-0_2_1".dir   = "0_2_1";
+  inputs."netty-0_2_1".owner = "nim-nix-pkgs";
+  inputs."netty-0_2_1".ref   = "master";
+  inputs."netty-0_2_1".repo  = "netty";
+  inputs."netty-0_2_1".type  = "github";
+  inputs."netty-0_2_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."netty-0_2_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."netpipe-v0_1_0".dir   = "v0_1_0";
+  inputs."netpipe-v0_1_0".owner = "nim-nix-pkgs";
+  inputs."netpipe-v0_1_0".ref   = "master";
+  inputs."netpipe-v0_1_0".repo  = "netpipe";
+  inputs."netpipe-v0_1_0".type  = "github";
+  inputs."netpipe-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."netpipe-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
